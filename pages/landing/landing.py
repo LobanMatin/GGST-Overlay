954fab4a5327594d8d417cf1b27e0f7b77119e36
Loban Matin <loban.matin@gmail.com>
2023-11-27 10:59:35 +1030

    finished landing page

--- pages/landing/landing.py
+++ pages/landing/landing.py
@@ -1,26 +1,30 @@
 import tkinter as tk
 from PIL import ImageTk, Image
 
-logo_img = Image.open("logo.png")
+logo_img = Image.open("pages/landing/logo.png")
+
 
 class LandingPage(tk.Frame):
     def __init__(self, controller, parent, root):
         tk.Frame.__init__(self, parent)
 
+        # images need to be global variables
+        global logo_img
+
         # create frame for buttons
         button_frame = tk.LabelFrame(self, bg="white", bd=0)
-        button_frame.place(relx=0, rely=0.5, anchor="n")
-        button_dims = (round(controller.width*0.05), round(controller.height*0.005)) #figure out dimensions
+        button_frame.place(relx=0.5, rely=0.5, anchor="n")
+        button_dims = (round(controller.width * 0.05), round(controller.height * 0.005))  # figure out dimensions
 
         # character select button
         char_button = tk.Button(button_frame, text="Character Select", width=button_dims[0], height=button_dims[1],
-                                pady=round(button_dims[1]/2),
+                                pady=round(button_dims[1] / 2),
                                 command=lambda: controller.page_to_top("CharSelect"))
-        char_button.grid(row=0, column=0, pady=25) # padding why 25?
+        char_button.grid(row=0, column=0, pady=25)  # padding why 25?
 
         # help/options button
         help_button = tk.Button(button_frame, text="Help/Options", width=button_dims[0], height=button_dims[1],
-                                pady=round(button_dims[1]/2),
+                                pady=round(button_dims[1] / 2),
                                 command=lambda: controller.page_to_top("Help"))
         help_button.grid(row=1, column=0, pady=25)
 
@@ -31,6 +35,11 @@ class LandingPage(tk.Frame):
         exit_button.grid(row=2, column=0, pady=25)
 
         # adding title to landing page
-        title_canvas = tk.Canvas(self, height=controller.height*0.2, width=controller.width*0.7, bg="white",
+        title_canvas = tk.Canvas(self, height=controller.height * 0.2, width=controller.width * 0.7, bg="white",
                                  highlightthickness=0)
-        
+
+        logo_img = logo_img.resize((round(controller.width * 0.7), round(controller.height * 0.2)), Image.LANCZOS)
+        logo_img = ImageTk.PhotoImage(logo_img, master=title_canvas)
+
+        title_canvas.create_image(0, 0, image=logo_img, anchor="nw")
+        title_canvas.place(relx=0.5, rely=0.1, anchor="n")
